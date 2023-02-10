b6224c4167d2b80c19c850509d8882c5bec1a510
Cristian Saenz <cristiansaenz@Cristians-MacBook-Pro.local>
2023-02-10 14:15:03 -0500

    Dogs Models and Temperaments Model defined

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,47 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      minHeight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      maxHeight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      minWeight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      maxWeight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      life_span: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
